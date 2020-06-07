55df2b898f6524abda3a1b07603662e03310ff80
Nimijamil333 <66129423+Nimijamil333@users.noreply.github.com>
2020-06-07 20:07:12 +0500

    Add files via upload

---
+++ APP.js
@@ -0,0 +1,173 @@
+//==================================================>>>>>>>  CHAPTER 1 <<<<<======================================================
+
+//     CHAPTER 1 QUESTION 1 
+// alert("HELLO USER ! ")
+
+//     CHAPTER 1 QUESTION 2
+// alert("Error! Please enter a valid password .")
+
+//     CHAPTER 1 QUESTION 3 
+// alert("welcome to js Land \n HAPPY CODING!" )
+
+//     CHAPTER 1 QUESTION 4
+// alert("Welcome to js land ")
+// alert("HAPPY CODING ! \n")//checkbox wala nhi hua remind me later ;/ 
+
+//==================================================>>>>>>>  CHAPTER 2 <<<<<======================================================
+
+//     CHAPTER 2 QUESTION 1 & 2 
+// var username ;
+// var myname;
+// myname="Nimra Jamil"
+
+//     CHAPTER 2 QUESTION 3
+// var message;
+// message = "hello world"
+// alert(message)
+//     CHAPTER 2 QUESTION 4
+// var name = "Mr.John"
+// var age = "i am 18 years old "
+// var qualification = " Certified Mobile Application Development" 
+// alert(name)
+// alert(age)
+// alert(qualification)
+
+//     CHAPTER 2 QUESTION 5
+// var pizza = "PIZZA"
+// alert(pizza +"\n"+ pizza.slice(0,4)+"\n"+pizza.slice(0,3)+"\n"+pizza.slice(0,2)+"\n"+pizza.slice(0,1));
+
+//     CHAPTER 2 QUESTION 6
+// var email = "nimijamil333@gmail.com"
+// alert("My email address is "+ email )
+
+//     CHAPTER 2 QUESTION 7
+// var book = "A smarter way to learn javaScript "
+// alert( "I am tryig to learn from the Book "+ book)
+
+//     CHAPTER 2 QUESTION 8
+// document.write("yeah! I can write HTML content through JavaScript ")
+//     CHAPTER 2 QUESTION 9 
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+//==================================================>>>>>>>  CHAPTER 3 <<<<<======================================================
+ 
+//    CHAPTER 3 QUESTION 1 
+// var age;
+// age = 19
+// alert("I am "+ age+" years old ")
+
+//    CHAPTER 3 QUESTION 2
+// var uservisit = 34
+// alert("You have visited this site "+uservisit+" times")
+
+//    CHAPTER 3 QUESTION 3
+// var Birthyear=2000
+// document.write("My birth year is "+Birthyear+"<br>Datatype of my Declared variable is Number")
+
+//     CHAPTER 3 QUESTION 4 
+// var visitorname="John Doe";
+// var product="T-Shirts";
+// var quantity=5;
+//  document.write(visitorname.bold() +" ordered "+ quantity +" " +product.bold()+" on XYZ clothing store ")
+
+//==================================================>>>>>>>  CHAPTER 4 <<<<<======================================================
+//    CHAPTER 4 QUESTION 1 
+//var a,b,c;
+
+//    CHAPTER 4 QUESTION 2
+// var a,b,c,d,e;//legal
+// var 1a,*b,3e,/f,(g);//illegal
+
+//    CHAPTER 4 QUESTION 3
+// var h="Rules for naming JS variables";
+// var ex="$my_1stVariable";
+// var n="$name, _name or name"
+// document.write(h.bold()+"<br><br><br>")
+// document.write("Variable names can only contain Numbers , $ & _ .  For example "+ ex.bold())
+// document.write("<br>Variables must begin with a $, letter or_. For example "+ n.bold())
+// document.write("<br>Variable names are case sensitive.")
+// document.write("<br>Variable names should not be JS Keywords.")
+
+//==================================================>>>>>>>  CHAPTER 5 <<<<<======================================================
+
+//     CHAPTER 5 QUESTION 1 & 2 
+// var num1= +prompt("Enter number : ")
+// var num2= +prompt("Enter 2nd number : ")
+// var sum = num1 + num2
+// var sub = num1 - num2
+// var mul = num1 * num2 
+// var div = num1 / num2
+// var mod = num1 % num2  
+// document.write("Sum of "+num1+ " & "+num2+" is "+sum);
+// document.write("<br>Subtraction of "+num1+ " & "+num2+" is "+sub);
+// document.write("<br>Multiplication of "+num1+ " & "+num2+" is "+mul);
+// document.write("<br>Division of "+num1+ " & "+num2+" is "+div);
+// document.write("<br>Mod of "+num1+ " & "+num2+" is "+mod);
+
+//     CHAPTER 5 QUESTION 3 
+// var a;
+// var v = "Value after variable declaration is : "
+// document.write(v.bold() + a)
+// a=5
+// document.write("<br>initial value : "+a)
+// a=++a
+// document.write("<br>value after increment : "+a)
+// a=a+7
+// document.write("<br>Value after addition : "+a)
+// a= --a
+// document.write("<br>Value after decrement: "+a)
+// var r=a%3
+// document.write("<br>remaainder is : "+r)
+
+//     CHAPTER 5 QUESTION 4
+// var movie_ticket=600;
+// var five=5*600
+// document.write("Total cost of buying 5 tickets to a movie is : "+five+"PKR") 
+
+//     CHAPTER 5 QUESTION 5
+// var num=4; 
+// var i ; 
+// for(i=1 ; i<=10 ; i++)
+// {
+//   document.write("<br>"+num + " x "+ i + "   =   " + i*num)
+// }
+
+//     CHAPTER 5 QUESTION 6
+// var celsius=20
+// var F = (celsius * (9/5)) +32
+// document.write(celsius+"°C is "+ F +"°f")
+// var fer = 100
+// var C= (fer -32 )*(5/9)
+// document.write("<br>"+fer+"°f is "+C+"°C .")
+
+//     CHAPTER 5 QUESTION 7
+// var price1 = 650;
+// var quantity1= 3;
+// var price2=100;
+// var quantity2=7;
+// var total1=price1*quantity1;
+// var total2=price2*quantity2;
+// var result=total1+total2 ;
+// var h ="SHOPPING CART";
+// document.write(h.bold()+"<br><br><br> Price of first item : "+price1+"<br>Quantity of first item : "+quantity1+"<br>Price of second item : "+price2+"<br>Quantity of second item : "+quantity2+"<br>Total : "+result)
+
+//     CHAPTER 5 QUESTION 8
+// var totalmarks=1050
+// var obtmarks=750
+// var per=(obtmarks*100)/totalmarks
+// var h= "Marks Sheet "
+// document.write(h.bold()+"<br><br><br>Total marks: "+totalmarks+"<br>Obtained marks : "+obtmarks+"<br>Percentage: "+per)
+
+//     CHAPTER 5 QUESTION 9
+// var $ = 10;
+// var riyal = 25;
+// var pak$ = 10*104.80;
+// var pakriyal = 25*28;
+// var ans = pak$ + pakriyal;
+// document.write("Total currency : " + ans+"PKR" )
+
+//     CHAPTER 5 QUESTION 10
+var n = 2;
+document.write("Ans : "+((n+5)*10)/2);
+
+//     CHAPTER 5 QUESTION 4
+//     CHAPTER 5 QUESTION 4
